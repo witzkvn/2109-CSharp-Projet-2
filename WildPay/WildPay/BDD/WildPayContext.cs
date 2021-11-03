7c50879d7b525c655cd2fb16a15699148f40e986
witzkvn <witz.kevin05@gmail.com>
2021-11-03 12:14:34 +0100

    debut mise en place db context

---
+++ WildPay/WildPay/BDD/WildPayContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WildPay.BDD
+{
+    public class WildPayContext : DbContext
+    {
+        public WildPayContext() : base("WildPayContext")
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
