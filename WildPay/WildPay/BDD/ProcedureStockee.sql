ca39dada1e3155dfe1eb15eaeb21700148aa1692
Sebastien David <sebastien.david08@gmail.com>
2021-11-04 15:59:41 +0100

    Ecriture procedures stockées INSERT

--- WildPay/WildPay/BDD/ProcedureStockee.sql
+++ WildPay/WildPay/BDD/ProcedureStockee.sql
@@ -1,1 +1,40 @@
-﻿
+﻿CREATE PROCEDURE sp_CreerUser
+@firstname VARCHAR(50),
+@lastname VARCHAR(50),
+@email VARCHAR(80),
+@password VARCHAR(80)
+AS
+BEGIN
+	INSERT INTO [User](Firstname,Lastname,Email,Password) VALUES
+	(@firstname, @lastname, @email, @password)
+END
+GO
+
+CREATE PROCEDURE sp_CreerCategory
+@name VARCHAR(100)
+AS
+BEGIN
+	INSERT INTO [Category](Name) VALUES
+	(@name)
+END
+GO
+
+CREATE PROCEDURE sp_CreerGroup
+@name VARCHAR(200)
+AS
+BEGIN
+	INSERT INTO [Group](Name) VALUES
+	(@name)
+END
+GO
+
+CREATE PROCEDURE sp_CreerExpense
+@date DATETIME,
+@title VARCHAR(200),
+@value DECIMAL(10,2)
+AS
+BEGIN
+	INSERT INTO [Expense](Date,Title,Value) VALUES
+	(@date, @title, @value)
+END
+GO
