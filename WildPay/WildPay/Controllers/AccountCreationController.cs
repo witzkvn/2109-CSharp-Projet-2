adb8630e47fb7150066281dc78718f6cf386181b
CleaH <clea.he@gmail.com>
2021-11-04 10:15:09 +0100

    creation controller account creation

---
+++ WildPay/WildPay/Controllers/AccountCreationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WildPay.Controllers
+{
+    public class AccountCreationController : Controller
+    {
+        // GET: AccountCreation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
