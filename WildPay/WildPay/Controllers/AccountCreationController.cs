fc888080d41fc4379136d5f2f278722201b5ca83
CleaH <clea.he@gmail.com>
2021-11-05 09:27:10 +0100

    adding htmlforms

--- WildPay/WildPay/Controllers/AccountCreationController.cs
+++ WildPay/WildPay/Controllers/AccountCreationController.cs
@@ -3,21 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WildPay.Models;
 
 namespace WildPay.Controllers
 {
     public class AccountCreationController : Controller
     {
         // GET: AccountCreation
-        public ActionResult Index()
+        public ActionResult Index(User newUser)
         {
+            if (ModelState.IsValid)
+            {
+                DAL.WildPayContext context = new DAL.WildPayContext();
+                context.Users.Add(newUser);
+                // Validate User
+                // retour à la page d'authentification
+                //return RedirectToAction("Index", "Home");
+            }
             return View();
         }
-        public void OnPost()
-        {
-            Console.WriteLine("hello ?");
-            Console.ReadLine();
-        }
+
+        //public ActionResult CreateUser(User newUser)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // Validate User
+        //        // retour à la page d'authentification
+        //        return RedirectToAction("Index", "Home");
+        //    }
+        //    return View();
+        //}
 
     }
 }
