bf16f00fc9adf752e08aff8ed9b25be0b70756e2
witzkvn <witz.kevin05@gmail.com>
2021-11-05 15:46:15 +0100

    ajout procedures stockées de recuperation d'un user par id et par email

--- WildPay/WildPay/BDD/ProcedureStockee.sql
+++ WildPay/WildPay/BDD/ProcedureStockee.sql
@@ -1,4 +1,5 @@
-﻿CREATE PROCEDURE sp_CreerUser
+﻿-- USER
+CREATE PROCEDURE sp_CreerUser
 @firstname VARCHAR(50),
 @lastname VARCHAR(50),
 @email VARCHAR(80),
@@ -10,32 +11,30 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE sp_GetUserByEmail
+	@UserEmail VARCHAR(100)
+   AS
+   BEGIN
+    SELECT * FROM [WildPay-1].[dbo].[User] WHERE Email = @UserEmail;
+   END
+GO
+
+CREATE PROCEDURE sp_GetUserById
+	@UserId INT
+   AS
+   BEGIN
+    SELECT * FROM [WildPay-1].[dbo].[User] WHERE Id = @UserId;
+   END
+GO
+
+
+-- CATEGORY
 CREATE PROCEDURE sp_CreerCategory
 @name VARCHAR(100)
 AS
 BEGIN
 	INSERT INTO [Category](Name) VALUES
 	(@name)
-END
-GO
-
-CREATE PROCEDURE sp_CreerGroup
-@name VARCHAR(200)
-AS
-BEGIN
-	INSERT INTO [Group](Name) VALUES
-	(@name)
-END
-GO
-
-CREATE PROCEDURE sp_CreerExpense
-@date DATETIME,
-@title VARCHAR(200),
-@value DECIMAL(10,2)
-AS
-BEGIN
-	INSERT INTO [Expense](Date,Title,Value) VALUES
-	(@date, @title, @value)
 END
 GO
 
@@ -47,3 +46,26 @@ DELETE FROM [Category]
 WHERE Id = @CategoryId
 END
 GO
+
+-- GROUP
+CREATE PROCEDURE sp_CreerGroup
+@name VARCHAR(200)
+AS
+BEGIN
+	INSERT INTO [Group](Name) VALUES
+	(@name)
+END
+GO
+
+-- EXPENSE
+CREATE PROCEDURE sp_CreerExpense
+@date DATETIME,
+@title VARCHAR(200),
+@value DECIMAL(10,2)
+AS
+BEGIN
+	INSERT INTO [Expense](Date,Title,Value) VALUES
+	(@date, @title, @value)
+END
+GO
+
