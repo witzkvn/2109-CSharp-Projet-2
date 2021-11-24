9ef7a2342029373f4529ab84feff9c57dd4437a7
witzkvn <witz.kevin05@gmail.com>
2021-11-24 16:43:29 +0100

    verification format et taille image avec message d'erreur

--- WildPay/WildPay/Controllers/AccountController.cs
+++ WildPay/WildPay/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ namespace WildPay.Controllers
     {
         private WildPayContext db = new WildPayContext();
 
-        public ActionResult Index()
+        public ActionResult Index(string error = null)
         {
             if (Session["Id"] == null)
             {
@@ -30,6 +30,8 @@ namespace WildPay.Controllers
             {
                 user.UserImageFile = ConverterTools.ByteArrayToStringImage(user.UserImage);
             }
+
+            ViewBag.ImageUploadMessage = error;
             return View(user);
         }
 
@@ -37,6 +39,7 @@ namespace WildPay.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index([Bind(Include = "Id, Firstname, Lastname, NewUserImageFile")] User user)
         {
+            string updateMessage = "";
             using (WildPayContext db = new WildPayContext())
             {
                 if (ModelState.IsValidField("Firstname") && ModelState.IsValidField("Lastname"))
@@ -46,21 +49,25 @@ namespace WildPay.Controllers
                         new SqlParameter("@firstname", user.Firstname),
                         new SqlParameter("@lastname", user.Lastname)
                     );
+                } else
+                {
+                    updateMessage += "Le nom où le prénom ne sont pas valides\n";
                 }
 
-                if (user.NewUserImageFile != null)
+                if (user.NewUserImageFile != null && FormatTools.VerifyImageFormatAndSize(user.NewUserImageFile))
                 {
                     byte[] data = ConverterTools.FileToByteArray(user.NewUserImageFile);
-
+                    
                     db.Database.ExecuteSqlCommand("sp_UpdateUserImageById @UserId, @ImageFile",
                         new SqlParameter("@UserId", Session["Id"]),
                         new SqlParameter("@ImageFile", data)
                     );
-
-                    //user.UserImageFile = ConverterTools.ByteArrayToStringImage(data);
+                } else if(user.NewUserImageFile != null)
+                {
+                    updateMessage += "Une erreur s'est produite. L'image doit être au format jpg, jpeg ou png, et ne pas dépasser 2 Mo.";
                 }
             }                
-            return RedirectToAction("Index", "Account");
+            return RedirectToAction("Index", "Account", new { error = updateMessage });
         }
     }
 }
