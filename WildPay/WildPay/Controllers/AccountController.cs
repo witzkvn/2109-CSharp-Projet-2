4702b4f1866ece978a6825b2bc02f72e6b11eb51
witzkvn <witz.kevin05@gmail.com>
2021-11-24 15:52:16 +0100

    update informations users avec appel procedures stockees

--- WildPay/WildPay/Controllers/AccountController.cs
+++ WildPay/WildPay/Controllers/AccountController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using WildPay.BDD;
 using WildPay.DAL;
 using WildPay.Models;
 using WildPay.Tools;
@@ -37,21 +37,30 @@ namespace WildPay.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index([Bind(Include = "Id, Firstname, Lastname, NewUserImageFile")] User user)
         {
-            using (Entities db = new Entities())
+            using (WildPayContext db = new WildPayContext())
             {
                 if (ModelState.IsValidField("Firstname") && ModelState.IsValidField("Lastname"))
                 {
-                    db.sp_UpdateUser(Session["Id"].ToString(), user.Firstname, user.Lastname);
+                    db.Database.ExecuteSqlCommand("sp_UpdateUser @UserId, @firstname, @lastname",
+                        new SqlParameter("@UserId", Session["Id"]),
+                        new SqlParameter("@firstname", user.Firstname),
+                        new SqlParameter("@lastname", user.Lastname)
+                    );
                 }
 
                 if (user.NewUserImageFile != null)
                 {
                     byte[] data = ConverterTools.FileToByteArray(user.NewUserImageFile);
-                    db.sp_UpdateUserImageById(Session["Id"].ToString(), data);
-                    user.UserImageFile = ConverterTools.ByteArrayToStringImage(data);
+
+                    db.Database.ExecuteSqlCommand("sp_UpdateUserImageById @UserId, @ImageFile",
+                        new SqlParameter("@UserId", Session["Id"]),
+                        new SqlParameter("@ImageFile", data)
+                    );
+
+                    //user.UserImageFile = ConverterTools.ByteArrayToStringImage(data);
                 }
             }                
-            return View("Index", user);
+            return RedirectToAction("Index", "Account");
         }
     }
 }
