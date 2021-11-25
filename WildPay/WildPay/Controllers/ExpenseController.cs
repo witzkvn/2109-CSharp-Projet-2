ff7f7fa02384ce8b92d94d6a21000a7891f15574
Sebastien David <sebastien.david08@gmail.com>
2021-11-25 16:33:04 +0100

    Creation vue et controller Expense

---
+++ WildPay/WildPay/Controllers/ExpenseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WildPay.DAL;
+using WildPay.Models;
+
+namespace WildPay.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        // GET: Expense
+        public ActionResult Index()
+        {
+            using (WildPayContext db = new WildPayContext())
+            {
+            
+            }
+            return View();
+        }
+    }
+}
