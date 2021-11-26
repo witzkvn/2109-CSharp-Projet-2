5debe8786872d8d924b6e04f8f8271cc74dd301c
Sebastien David <sebastien.david08@gmail.com>
2021-11-26 15:00:28 +0100

    Creation model, controller et view pour afficher la liste de dépenses via la procédure stockée

--- WildPay/WildPay/Controllers/ExpenseController.cs
+++ WildPay/WildPay/Controllers/ExpenseController.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WildPay.DAL;
 using WildPay.Models;
 
+
 namespace WildPay.Controllers
 {
     public class ExpenseController : Controller
     {
+        WildPayContext db = new WildPayContext();
         // GET: Expense
         public ActionResult Index()
         {
-            using (WildPayContext db = new WildPayContext())
-            {
-            
-            }
+
+            var returnCode = new SqlParameter();
+            returnCode.ParameterName = "@GroupID ";
+            returnCode.SqlDbType = SqlDbType.Int;
+            returnCode.Direction = ParameterDirection.Output;
+
+            db.Database.ExecuteSqlCommand("sp_GetGroupePrincipalId @GroupID OUTPUT", returnCode);
+
+            int GroupId = (int)returnCode.Value;
+
+            List<ExpenseCategoryJoin> expenses = db.Database.SqlQuery<ExpenseCategoryJoin>
+                ("sp_GetExpense @p0", GroupId)
+                .ToList();
+
+            ViewBag.listExpenses = expenses;
+
+
             return View();
         }
     }
---
+++ WildPay/WildPay/Models/ExpenseCategoryJoin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WildPay.Tools;
+
+namespace WildPay.Models
+{
+    public class ExpenseCategoryJoin
+    {
+        public DateTime CreatedAt { get; set; }
+        public string Title { get; set; }
+        public double Value { get; set; }
+        public int FkUserId { get; set; }
+        public int FkCategoryId { get; set; }
+        public int FkGroupId { get; set; }
+        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public byte[] UserImage { get; set; }
+        public string DateCourte { get; set; }
+
+        //public ExpenseCategoryJoin ()
+        //{
+        //    DateCourte = FormatTools.ConvertInShortDate(CreatedAt);
+        //}
+    }
+}
