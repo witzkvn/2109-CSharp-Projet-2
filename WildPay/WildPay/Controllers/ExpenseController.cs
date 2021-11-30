785de057c78cef3db507f47bb9ee3b3fa22e348c
Sebastien David <sebastien.david08@gmail.com>
2021-11-30 23:13:34 +0100

    Creation des viewbags dans le controller Expense pour afficher la somme due

--- WildPay/WildPay/Controllers/ExpenseController.cs
+++ WildPay/WildPay/Controllers/ExpenseController.cs
@@ -14,11 +14,10 @@ namespace WildPay.Controllers
     public class ExpenseController : Controller
     {
         WildPayContext db = new WildPayContext();
+        int GroupId = Utilities.GetGroupePrincipalId();
         // GET: Expense
         public ActionResult Index()
         {
-            int GroupId = Utilities.GetGroupePrincipalId();
-
             List<ExpenseCategoryJoin> expenses = db.Database.SqlQuery<ExpenseCategoryJoin>
                 ("sp_GetExpense @p0", GroupId)
                 .ToList();
@@ -32,7 +31,56 @@ namespace WildPay.Controllers
             }
             ViewBag.listExpenses = expenses;
 
+            List<ListUser> user = db.Database.SqlQuery<ListUser>
+                    ("sp_GetUser @p0", GroupId)
+                    .ToList();
+
+            ViewBag.listeUsers = user;
+            ViewBag.SommesDues = SommesDues(ViewBag.listeUsers);
+
             return View();
         }
+
+        private Dictionary<int, double> SommesDues(List<ListUser> listeUser)
+        {
+            Dictionary<int, double> sommesDues = new Dictionary<int, double>();
+            
+            foreach(ListUser user in listeUser)
+            {
+                double sommeDueParUser = 0;
+                double sommeDueParUserShort = 0;
+                var returnSomme = new SqlParameter();
+                returnSomme.ParameterName = "@sommeDue ";
+                returnSomme.SqlDbType = SqlDbType.Money;
+                returnSomme.Direction = ParameterDirection.Output;
+
+                db.Database.ExecuteSqlCommand("sp_GetSommeDue @UserId, @groupId, @sommeDue OUTPUT",
+                    new SqlParameter("@UserId", user.Id),
+                    new SqlParameter("@groupId", GroupId),
+                    returnSomme);
+
+                string returnSommeToString = returnSomme.Value.ToString();
+
+                if(returnSommeToString != "")
+                {
+                    sommeDueParUser = Convert.ToDouble(returnSommeToString);
+                    sommeDueParUserShort = FormatTools.ConvertinShortDouble(sommeDueParUser);
+                }
+                else
+                {
+                    db.Database.ExecuteSqlCommand("sp_GetSommeDueSiNull @groupId, @sommeDue OUTPUT",
+                    new SqlParameter("@groupId", GroupId),
+                    returnSomme);
+                    string returnSommeToStringSiNull = returnSomme.Value.ToString();
+                    sommeDueParUser = Convert.ToDouble(returnSommeToStringSiNull);
+                    sommeDueParUserShort = FormatTools.ConvertinShortDouble(sommeDueParUser);
+                }
+
+                sommesDues.Add(user.Id, sommeDueParUserShort);
+            }
+
+            return sommesDues;
+        }
+
     }
 }
