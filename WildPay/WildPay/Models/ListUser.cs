a3b0cc337f4740e3e7e4e7b15a98bc3ac07e6123
Sebastien David <sebastien.david08@gmail.com>
2021-11-30 23:14:58 +0100

    Creation de la class ListUser dans Models afin de pouvoir récupérer la liste des users à utiliser pour afficher la somme due

---
+++ WildPay/WildPay/Models/ListUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WildPay.Models
+{
+    public class ListUser
+    {
+        public int Id { get; private set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
