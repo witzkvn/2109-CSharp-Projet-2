599c86af4babb1f02e46e14140fd2453ebf6f459
CleaH <clea.he@gmail.com>
2021-12-08 17:12:27 +0100

    model changes

--- WildPay/WildPay/Models/Expense.cs
+++ WildPay/WildPay/Models/Expense.cs
@@ -19,7 +19,8 @@ namespace WildPay.Models
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
         [Required(ErrorMessage = "Le titre de la dépense est obligatoire")]
-        [MaxLength(50, ErrorMessage = "Le nom doit faire 50 caractères au maximum"), MinLength(3, ErrorMessage ="Le nom doit faire 3 caractères au minimum")]
+        [MaxLength(30, ErrorMessage = "Le titre de la dépense doit faire 30 caractères au maximum")]
+        [MinLength(2, ErrorMessage = "Le titre de la dépense doit faire 2 caractères au minimum")]
         [DisplayName("Titre")]
         public string Title { get; set; }
 
